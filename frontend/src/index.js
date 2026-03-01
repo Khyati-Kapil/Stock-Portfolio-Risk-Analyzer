218d2d9f4b9a000017225f621d9dbe3305afd9d6
Khyati-Kapil <khyatekapil@gmail.com>
2026-03-01 11:25:51 +0530

    prep for render deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import { AuthProvider } from './auth/AuthContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <AuthProvider>
                 <App />
             </AuthProvider>
